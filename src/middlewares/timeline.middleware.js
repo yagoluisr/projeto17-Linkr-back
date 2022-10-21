2ccfec3e90ec01e0098d9131565b8ce1d7974358
carlos_casanova <carlosctu94@gmail.com>
2022-10-21 01:38:00 -0300

    [FEAT]LINKR02/Modifing queries & returns

--- src/middlewares/timeline.middleware.js
+++ src/middlewares/timeline.middleware.js
@@ -1,15 +1,17 @@
-import { badRequestResponse } from "../controllers/controllers.helper.js";
+import * as responses from "../controllers/controllers.helper.js";
 import { getPost } from "../repositories/timeline.repository.js";
 
 async function validatePost(req, res, next) {
   const { id } = req.params;
+  const { user_id } = res.locals;
 
-  if (isNaN(parseInt(id))) return badRequestResponse(res);
+  if (isNaN(parseInt(id))) return responses.badRequestResponse(res);
 
   try {
-    const postExists = (await getPost(id)).rowCount;
+    const postExists = (await getPost({ id, user_id })).rowCount;
 
-    if (postExists === 0) return badRequestResponse(res, "Post not found");
+    if (postExists === 0)
+      return responses.unauthorizedResponse(res, "Post not found");
   } catch (error) {
     serverErrorResponse(res, error);
   }
