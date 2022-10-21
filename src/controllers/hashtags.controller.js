191d67b6042c58d65b818659fbb96c167316e0e8
ThiagoCCR <thiago.ccr96@gmail.com>
2022-10-21 10:00:35 -0300

    [FEAT] authReq && fix getTrendingHashtags && remove await from hashtagsControllers

--- src/controllers/hashtags.controller.js
+++ src/controllers/hashtags.controller.js
@@ -2,13 +2,14 @@ import { serverErrorResponse, okResponse } from "./controllers.helper.js";
 
 import { getTrends } from "../repositories/hashtags.repository.js";
 
-async function GetTrendingHashtags(req, res) {
+async function getTrendingHashtags(req, res) {
+
   try {
     const trendingHashtags = await getTrends();
-    okResponse(res, trendingHashtags.rows[0]);
+    okResponse(res, trendingHashtags.rows);
   } catch (error) {
     serverErrorResponse(res, error);
   }
 }
 
-export { GetTrendingHashtags };
+export { getTrendingHashtags };
--- src/routers/hashtags.router.js
+++ src/routers/hashtags.router.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { GetTrendingHashtags } from "../controllers/hashtags.controller.js";
+import { getTrendingHashtags } from "../controllers/hashtags.controller.js";
+import checkHeader from "../middlewares/check.header.js"
 
 const hashtagsRouter = Router();
 
-hashtagsRouter.get("/trending", GetTrendingHashtags);
+hashtagsRouter.get("/trending", checkHeader, getTrendingHashtags);
 
 export default hashtagsRouter;
