27dfedd155c8ba891633e413afa1d894b6a4539e
ThiagoCCR <thiago.ccr96@gmail.com>
2022-10-23 16:57:17 -0300

    fix regex && added DESC on getTrends

--- src/middlewares/check.hashtags.middleware.js
+++ src/middlewares/check.hashtags.middleware.js
@@ -1,7 +1,7 @@
 async function checkForHashtags(req, res, next) {
   const { description } = res.locals.body;
 
-  const hashtags = description.match(/#[^\s#]*/gim);
+  const hashtags = description?.match(/#[^\s#\.\;]*/gmi);
 
   res.locals.hashtags = hashtags;
 
