2f927e501d9af142ac5ef6c5bb8fa6a486d8bac6
ThiagoCCR <thiago.ccr96@gmail.com>
2022-10-24 12:39:31 -0300

    fix regex

--- src/middlewares/check.hashtags.middleware.js
+++ src/middlewares/check.hashtags.middleware.js
@@ -1,7 +1,7 @@
 async function checkForHashtags(req, res, next) {
   const { description } = res.locals.body;
 
-  const hashtags = description?.match(/#[^\s#\.\;]*/gmi);
+  const hashtags = description?.match(/#[a-zA-ZÀ-ÿ0-9_-]+/g);
 
   res.locals.hashtags = hashtags;
 
