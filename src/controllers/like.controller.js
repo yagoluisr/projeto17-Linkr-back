6dca8925a0133a75a327d58b2fa1d345b163e1f4
carlos_casanova <carlosctu94@gmail.com>
2022-10-24 01:51:34 -0300

    [FEAT]Adding Likestooltip

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -1,50 +1,63 @@
-import { deleteLike, getPostLikedByUser, insertLike, getPostLikesNumber } from "../repositories/like.repository.js";
+import {
+  deleteLike,
+  getPostLikedByUser,
+  insertLike,
+  getPostLikesNumber,
+  getUsersLikesPost,
+} from "../repositories/like.repository.js";
 import * as responses from "./controllers.helper.js";
 
 async function getLike(req, res) {
-    const { user, id } = res.locals;
+  const { user, id } = res.locals;
 
-    try {
-        const like = await getPostLikedByUser(user.id, id);
-        const likesNumber = await getPostLikesNumber(id);
+  try {
+    const like = await getPostLikedByUser(user.id, id);
+    const likesNumber = await getPostLikesNumber(id);
+    let usersLikePost = (await getUsersLikesPost(id)).rows.map(
+      (user) => user.name
+    );
 
-        if (!like.rows[0]) {
-            responses.okResponse(res, { likedByUser: false, ...likesNumber.rows[0] });
-            return;
-        }
-
-        responses.okResponse(res, { likedByUser: true, ...likesNumber.rows[0] });
-    } catch (error) {
-        responses.serverErrorResponse(res, error);
+    if (!like.rows[0]) {
+      responses.okResponse(res, {
+        likedByUser: false,
+        ...likesNumber.rows[0],
+        users: [usersLikePost],
+      });
+      return;
     }
+
+    responses.okResponse(res, {
+      likedByUser: true,
+      ...likesNumber.rows[0],
+      users: [usersLikePost],
+    });
+  } catch (error) {
+    responses.serverErrorResponse(res, error);
+  }
 }
 
 async function likePost(req, res) {
-    const { user, id } = res.locals;
+  const { user, id } = res.locals;
 
-    try {
-        await insertLike(user.id, id);
+  try {
+    await insertLike(user.id, id);
 
-        responses.okResponse(res);        
-    } catch (error) {
-        responses.serverErrorResponse(res, error);
-    }
+    responses.okResponse(res);
+  } catch (error) {
+    responses.serverErrorResponse(res, error);
+  }
 }
 
 async function dislikePost(req, res) {
-    const { user, id } = res.locals;
+  const { user, id } = res.locals;
 
-    try {
-        await deleteLike(user.id, id);
+  try {
+    await deleteLike(user.id, id);
 
-        responses.noContentResponse(res);        
-    } catch (error) {
-        responses.serverErrorResponse(res, error);
-    }
+    responses.noContentResponse(res);
+  } catch (error) {
+    responses.serverErrorResponse(res, error);
+  }
 }
 
-export {
-    getLike,
-    likePost,
-    dislikePost
-};
+export { getLike, likePost, dislikePost };
